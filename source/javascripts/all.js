5d3bedd5008434e530c4514976ca2fff5c254d55
Matteo Abis <gmatteo..abis@gmail.com>
2015-04-16 10:25:58 +0200

    add pilatus experiments

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,2 +1,8 @@
 //= require reveal.js/js/reveal
 //= require reveal_init
+//= require d3.base.chart/base.chart
+//= require d3.line/line
+//= require d3.line.legend/line.legend
+//= require d3.axes/axes
+//= require efficiency
+//= require visibility
